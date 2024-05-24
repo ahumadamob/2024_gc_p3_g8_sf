7e184502208334d449b7a7fd1f682ea233d1726a
Luciano Badilla <lucianobadilla88@gmail.com>
2024-05-24 13:43:00 -0300

    Correciones y Atributos agregados.

--- src/main/java/gr8/imb3/progra3/entity/Producto.java
+++ src/main/java/gr8/imb3/progra3/entity/Producto.java
@@ -13,22 +13,24 @@ public class Producto {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	private String cantidad;
+	private String Descripcion;
+	private String CodigoDeBarra;
+	private Boolean Disponibilidad;
 	@ManyToOne
 	@JoinColumn(name = "idCategoria")
 	private Categoria categoria;
+	private Integer cantidad;
 	
-	
-	public Integer getIdProducto() {
+	public Integer getId() {
 		return id;
 	}
-	public void setIdProducto(Integer idProducto) {
+	public void setId(Integer idProducto) {
 		this.id = idProducto;
 	}
-	public String getCantidad() {
+	public Integer getCantidad() {
 		return cantidad;
 	}
-	public void setCantidad(String cantidad) {
+	public void setCantidad(Integer cantidad) {
 		this.cantidad = cantidad;
 	}
 	public void setCategoria(Categoria categoria) {
@@ -36,6 +38,24 @@ public class Producto {
 	}
 	public Categoria getCategoria() {
 		return categoria;
+	}
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	public Boolean getDisponibilidad() {
+		return Disponibilidad;
+	}
+	public void setDisponibilidad(Boolean disponibilidad) {
+		Disponibilidad = disponibilidad;
+	}
+	public String getCodigoDeBarra() {
+		return CodigoDeBarra;
+	}
+	public void setCodigoDeBarra(String codigoDeBarra) {
+		CodigoDeBarra = codigoDeBarra;
 	}
 	
 
