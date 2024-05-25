bbfe3870123957386a32164f9961fe08e3046e23
FacundoCortez20 <facundocortezroa@gmail.com>
2024-05-25 18:02:12 -0300

    Cambios pedidos

--- src/main/java/gr8/imb3/progra3/entity/Proveedor.java
+++ src/main/java/gr8/imb3/progra3/entity/Proveedor.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Entity
 public class Proveedor {
@@ -16,6 +22,21 @@ public class Proveedor {
 	@Size(max = 50, message = "El nombre no debe superar los 50 caracteres") 
 	private String  nombre;
 	private boolean habilitado;
+	private String direccion;
+	private Integer telefono;
+	private String correoElectronico;
+	private Integer cuil;
+	private String personaContacto;
+	private String comentario;
+@ManyToMany
+@JoinTable(name="categoria_proveedor",
+		joinColumns = {@JoinColumn(name = "proveedor_id")},
+		inverseJoinColumns = {@JoinColumn(name = "categoria_id")})
+private Set<Categoria> categoria = new HashSet<>();
+
+		
+	
+	
 	public Integer getId() {
 		return id;
 	}
@@ -33,5 +54,41 @@ public class Proveedor {
 	}
 	public void setHabilitado(boolean habilitado) {
 		this.habilitado = habilitado;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public Integer getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(Integer telefono) {
+		this.telefono = telefono;
+	}
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+	public Integer getCuil() {
+		return cuil;
+	}
+	public void setCuil(Integer cuil) {
+		this.cuil = cuil;
+	}
+	public String getPersonaContacto() {
+		return personaContacto;
+	}
+	public void setPersonaContacto(String personaContacto) {
+		this.personaContacto = personaContacto;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
 	}
 }
